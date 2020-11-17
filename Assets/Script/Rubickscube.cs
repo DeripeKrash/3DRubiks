c4b08c4585bb4e6ce54dcc40a5330ca1b168c8d8
Denis Petit <d.petit@student.isartdigital.com>
2020-11-17 17:10:03 +0100

    Basic Cube cut

--- Assets/Script/Rubickscube.cs
+++ Assets/Script/Rubickscube.cs
@@ -4,11 +4,44 @@ using UnityEngine;
 
 public class Rubickscube : MonoBehaviour
 {
+    [SerializeField] uint size = 3;
+
+    [SerializeField] VisibleCube refCube;
+
+    [SerializeField] List<VisibleCube> visibleCubes;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        float U = (0.5f * (size - 1)) / size;
+
+        for (uint i = 0; i < size; i++)
+        {
+            for (uint j = 0; j < size; j++)
+            {
+                for (uint k = 0; k < size; k++)
+                {
+                    if (i == 0 || j == 0 || k == 0 || i == size - 1 || j == size - 1 || k == size - 1)
+                    {
+
+                        VisibleCube newCube = Instantiate(refCube);
+
+                        newCube.transform.localScale = new Vector3(1.0f / size, 1.0f / size, 1.0f / size);
+                        newCube.transform.parent = transform;
+
+                        newCube.transform.position = new Vector3(i * (1.0f / size), j * (1.0f / size), k * (1.0f / size));
+                        newCube.transform.position -= new Vector3(U, U, U);
+
+                        newCube.SetPositionOnCube(i, j, k);
+
+                        visibleCubes.Add(newCube);
+                    }
+                }
+            }
+        }
     }
+
 
     // Update is called once per frame
     void Update()
---
+++ Assets/Script/VisibleCube.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisibleCube : MonoBehaviour
+{
+    public uint i;
+    public uint j;
+    public uint k;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetPositionOnCube(uint I, uint J, uint K)
+    {
+        i = I;
+        j = J;
+        k = K;
+    }
+}
