306cc7ac1199d93a44cd52118774b661ec4a23c3
wil <william.nardone@hotmail.fr>
2020-11-17 17:06:19 +0100

    +Add Camera controles zqsdSpaceLeftCtrl for strafe and hold right click to rotate camera.

---
+++ Assets/SetupCamInput.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetupCamInput : MonoBehaviour
+{
+    [SerializeField] private float speed = 1;
+    [SerializeField] private float mouseSpeed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0 || Input.GetAxis("Height") != 0)
+        {
+            Vector3 newPos = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Height"), Input.GetAxis("Vertical"));
+            transform.Translate(newPos * speed * Time.deltaTime, Space.Self);
+        }
+
+        if (Input.GetMouseButton(1))
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            transform.Rotate(transform.right    , Input.GetAxis("Mouse Y") * mouseSpeed * Time.deltaTime, Space.World);
+            transform.Rotate(new Vector3(0,1,0) , Input.GetAxis("Mouse X") * mouseSpeed * Time.deltaTime, Space.World);
+        }
+
+        else
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}
