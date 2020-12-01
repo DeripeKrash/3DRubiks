18067285a7599b71504ac7b8f5646b9f3edfe994
wil <william.nardone@hotmail.fr>
2020-12-01 16:33:35 +0100

    no message

--- Assets/Script/Rubickscube.cs
+++ Assets/Script/Rubickscube.cs
@@ -10,6 +10,13 @@ public class Rubickscube : MonoBehaviour
 
     [SerializeField] List<VisibleCube> visibleCubes;
 
+    [SerializeField] Material Color1;
+    [SerializeField] Material Color2;
+    [SerializeField] Material Color3;
+    [SerializeField] Material Color4;
+    [SerializeField] Material Color5;
+    [SerializeField] Material Color6;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -28,12 +35,29 @@ public class Rubickscube : MonoBehaviour
                         VisibleCube newCube = Instantiate(refCube);
 
                         newCube.transform.localScale = new Vector3(1.0f / size, 1.0f / size, 1.0f / size);
-                        newCube.transform.parent = transform;
+                        
+                        newCube.transform.parent = transform; // Put new Cube under the Rubicks Cube
 
-                        newCube.transform.position = new Vector3(i * (1.0f / size), j * (1.0f / size), k * (1.0f / size));
-                        newCube.transform.position -= new Vector3(U, U, U);
+                        newCube.transform.position = transform.position;
+
+                        newCube.transform.GetChild(0).position = new Vector3(i * (1.0f / size), j * (1.0f / size), k * (1.0f / size));
+                        newCube.transform.GetChild(0).position -= new Vector3(U, U, U);
 
                         newCube.SetPositionOnCube(i, j, k);
+
+                        
+                        if (i == 0)
+                        {
+                            newCube.AddFace(Vector3.forward, Color1);
+                        }
+                        /*if (j == 0)
+                        {
+                            newCube.AddFace(Vector3.up, Color2);
+                        }
+                        if (k == 0)
+                        {
+                            newCube.AddFace(Vector3.forward, Color3);
+                        }*/
 
                         visibleCubes.Add(newCube);
                     }
@@ -47,5 +71,36 @@ public class Rubickscube : MonoBehaviour
     void Update()
     {
         
+        //RotateLine(2, 1);
+    }
+
+    /*void RotateLine(uint J, float angle)
+    {
+        for (int i = 0; i < visibleCubes.Count; i++)
+        {
+            if (visibleCubes[i].j == J)
+            {
+                visibleCubes[i].transform.Rotate(new Vector3(0,angle,0));
+            }
+
+        }
+    }*/
+
+    IEnumerator RotateLine(float duration)
+    {
+        float actualTime = 0;
+        float lastTime = Time.time;
+
+        while (duration - actualTime > 0)
+        {
+            actualTime += Time.time - lastTime;
+            lastTime = Time.time;
+
+
+
+            yield return new WaitForEndOfFrame();
+        }
+
+        yield break;
     }
 }
--- Assets/Script/VisibleCube.cs
+++ Assets/Script/VisibleCube.cs
@@ -2,11 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+struct Face
+{
+    Vector3 normal;
+    uint color;
+};
+
 public class VisibleCube : MonoBehaviour
 {
     public uint i;
     public uint j;
     public uint k;
+
+    List<Face> listFaces;
 
     // Start is called before the first frame update
     void Start()
@@ -20,10 +28,32 @@ public class VisibleCube : MonoBehaviour
         
     }
 
+    public void AddFace(Vector3 normal, uint color)
+    {
+        //listFaces.Add({normal, color});
+    }
+
     public void SetPositionOnCube(uint I, uint J, uint K)
     {
         i = I;
         j = J;
         k = K;
+    }
+
+    public void AddFace(Vector3 FaceVector, Material mat)
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            if (transform.GetChild(0).GetChild(i).forward == FaceVector)
+            {
+                Renderer rend = transform.GetChild(0).GetChild(i).GetComponent<Renderer>();
+                //transform.GetChild(i).GetComponent < Material > = mat;
+
+                if (rend)
+                {
+                    rend.material = mat;
+                }
+            }
+        }
     }
 }
