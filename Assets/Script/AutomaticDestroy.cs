d1164ff9fd1299f4a18109c054de3903bf37a4eb
Denis Petit <d.petit@student.isartdigital.com>
2020-12-13 18:10:33 +0100

    Save

---
+++ Assets/Script/AutomaticDestroy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutomaticDestroy : MonoBehaviour
+{
+    [SerializeField] float destructionTime = 0.5f;
+    float creationTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        creationTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - creationTime >= destructionTime)
+        {
+            Destroy(this);
+        }
+    }
+}
--- Assets/Script/VisibleCube.cs
+++ Assets/Script/VisibleCube.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public struct Face
+{
+    public uint color;
+    public Transform trsFace;
+};
+
 public class VisibleCube : MonoBehaviour
 {
-    public uint i;
-    public uint j;
-    public uint k;
+    List<Face> listFaces = new List<Face>();
 
     // Start is called before the first frame update
     void Start()
@@ -17,13 +21,50 @@ public class VisibleCube : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    public void SetPositionOnCube(uint I, uint J, uint K)
+    public void AddFace(List<Transform> list, Vector3 FaceVector, Material mat, uint color)
     {
-        i = I;
-        j = J;
-        k = K;
+        for (int i = 0; i < transform.GetChild(0).childCount; i++)
+        {
+            if ((transform.GetChild(0).GetChild(i).forward - FaceVector).magnitude < 0.2)
+            {
+                
+                Renderer rend = transform.GetChild(0).GetChild(i).GetComponent<Renderer>();
+
+                if (rend)
+                {
+                    rend.material = mat;
+                }
+
+                Face newFace = new Face();
+                newFace.trsFace = transform.GetChild(0).GetChild(i);
+                newFace.color = color;
+
+                listFaces.Add(newFace);
+                list.Add(transform.GetChild(0).GetChild(i));
+
+            }
+        }
+    }
+
+    public void Clean()
+    {
+        for (int i = 0; i < transform.GetChild(0).childCount; i++)
+        {
+            bool clear = true;
+            for (int e = 0; e < listFaces.Count; e++)
+            {
+                if (transform.GetChild(0).GetChild(i) == listFaces[e].trsFace)
+                {
+                    clear = false;
+                }
+            }
+            if (clear)
+            {
+                Destroy(transform.GetChild(0).GetChild(i).gameObject);
+            }
+        }
     }
 }
